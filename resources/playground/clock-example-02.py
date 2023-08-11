6e5970776169166b972b8e4c792e2b6f042e4781
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-08-11 19:35:10 +0200

    Adding clock examples.

---
+++ resources/playground/clock-example-02.py
@@ -0,0 +1,40 @@
+import asyncio
+import time
+from typing import Optional
+
+
+def now():
+	return time.time()
+
+
+class Worker:
+	def __init__(self, name, waiting_time):
+		self.name = name
+		self.waiting_time = waiting_time
+		self.timestamp: Optional[int] = None
+		self.event: Optional[asyncio.Event] = asyncio.Event()
+
+	async def start(self):
+		while True:
+			print(f"""{now()} {self.name}: loop wait""")
+			await self.event.wait()
+			print(f"""{now()} {self.name}: loop start""")
+			await asyncio.sleep(1)  # Doing async stuff
+			print(f"""{now()} {self.name}: loop end""")
+			await asyncio.sleep(self.waiting_time)
+
+
+async def main():
+	worker1 = Worker("worker1", 3)
+	worker2 = Worker("worker2", 30)
+
+	task1 = asyncio.create_task(worker1.start())
+	task2 = asyncio.create_task(worker2.start())
+
+	worker1.event.set()
+	worker2.event.set()
+
+	await asyncio.gather(task1, task2)
+
+if __name__ == "__main__":
+	asyncio.run(main())
