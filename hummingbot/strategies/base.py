c32bdcced66bea66ea9a31c64339f8bb183475f7
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-08-17 14:30:45 +0200

    Fixing logs (two f_back are needed because of the decorator to automatically log the input arguments of a method when we have an exception).

--- hummingbot/strategies/base.py
+++ hummingbot/strategies/base.py
@@ -14,9 +14,9 @@ class Base(ABC):
 	def log(self, level: int, message: str = "", object: Any = None):
 		# noinspection PyUnresolvedReferences
 		from logger import logger
-		logger.log(level=level, prefix=self.logger_prefix, message=message, object=object, frame=inspect.currentframe().f_back)
+		logger.log(level=level, prefix=self.logger_prefix, message=message, object=object, frame=inspect.currentframe().f_back.f_back)
 
 	def ignore_exception(self, exception: Exception):
 		# noinspection PyUnresolvedReferences
 		from logger import logger
-		logger.ignore_exception(prefix=self.logger_prefix, exception=exception, frame=inspect.currentframe().f_back)
+		logger.ignore_exception(prefix=self.logger_prefix, exception=exception, frame=inspect.currentframe().f_back.f_back)
