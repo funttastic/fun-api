d76414cf39fa2e6143a508b5bbb209c99a3aff56
Danilo Araújo Silva <danilo.silva@funttastic.com>
2023-08-29 20:01:05 +0200

    Small changes.

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ async def start_api():
 	logger.log(logging.INFO, f'Environment: {properties.get("environment")}')
 
 	host = os.environ.get("HOST", properties.get('server.host'))
-	port = os.environ.get("PORT", properties.get('server.port'))
+	port = int(os.environ.get("PORT", properties.get('server.port')))
 	environment = properties.get_or_default('server.environment', constants.environments.production)
 
 	os.environ['ENV'] = environment
