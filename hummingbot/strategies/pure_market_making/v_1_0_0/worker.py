2944f177df4d615a0a6f67a2595e58d956c2e88c
Álvaro E. F. Mota <alvaro.mota@funttastic.com>
2023-10-02 15:22:31 -0300

    Fixed if setences

--- hummingbot/strategies/pure_market_making/v_1_0_0/worker.py
+++ hummingbot/strategies/pure_market_making/v_1_0_0/worker.py
@@ -1059,25 +1059,29 @@ class Worker(WorkerBase):
 					if self._configuration.strategy.kill_switch.max_wallet_loss_from_initial_value:
 						if math.fabs(wallet_current_initial_pnl) >= math.fabs(max_wallet_loss_from_initial_value):
 							self.log(CRITICAL, f"""\n\nThe bot has been stopped because the wallet lost {format_percentage(wallet_current_initial_pnl, 3)}, which is at least {max_wallet_loss_from_initial_value}% distant from the wallet initial value.\n/cc {users}""")
-
 							await self.stop()
 
-					elif self._configuration.strategy.kill_switch.max_wallet_loss_from_previous_value:
+							return
+
+					if self._configuration.strategy.kill_switch.max_wallet_loss_from_previous_value:
 						if math.fabs(wallet_current_previous_pnl) >= math.fabs(max_wallet_loss_from_previous_value):
 							self.log(CRITICAL, f"""\n\nThe bot has been stopped because the wallet lost {format_percentage(wallet_current_previous_pnl, 3)}, which is at least {max_wallet_loss_from_previous_value}% distant from the wallet previous value.\n/cc {users}""")
-
 							await self.stop()
 
-					elif self._configuration.strategy.kill_switch.max_wallet_loss_compared_to_token_variation:
+							return
+
+					if self._configuration.strategy.kill_switch.max_wallet_loss_compared_to_token_variation:
 						if math.fabs(wallet_current_initial_pnl - token_base_current_initial_pnl) >= math.fabs(max_wallet_loss_compared_to_token_variation):
 							self.log(CRITICAL, f"""\n\nThe bot has been stopped because the wallet lost {format_percentage(wallet_current_initial_pnl, 3)}, which is at least {max_wallet_loss_compared_to_token_variation}% distant from the token price variation ({token_base_current_initial_pnl}) from its initial price.\n/cc {users}""")
-
 							await self.stop()
 
-				elif token_base_current_initial_pnl < 0 and math.fabs(token_base_current_initial_pnl) >= math.fabs(max_token_loss_from_initial):
-					self.log(CRITICAL, f"""\n\nThe bot has been stopped because the token lost {format_percentage(token_base_current_initial_pnl, 3)}, which is at least {max_token_loss_from_initial}% distant from the token initial price.\n/cc {users}""")
+							return
 
+				if token_base_current_initial_pnl < 0 and math.fabs(token_base_current_initial_pnl) >= math.fabs(max_token_loss_from_initial):
+					self.log(CRITICAL, f"""\n\nThe bot has been stopped because the token lost {format_percentage(token_base_current_initial_pnl, 3)}, which is at least {max_token_loss_from_initial}% distant from the token initial price.\n/cc {users}""")
 					await self.stop()
+
+					return
 		finally:
 			self.log(DEBUG, """end""")
 
