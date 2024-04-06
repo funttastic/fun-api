1b10828c21a19e9b1657b1cd33a5f7c943c8f6e8
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-04-06 20:54:21 -0300

    Improving extensions.

--- core/extensions.py
+++ core/extensions.py
@@ -1,6 +1,15 @@
 from dotmap import DotMap
-
 from core.utils import safe_deep_get, safe_deep_set
+
+original_init = DotMap.__init__
+
+
+def new_init(self, *args, **kwargs):
+	kwargs['_dynamic'] = False
+	original_init(self, *args, **kwargs)
+
+
+DotMap.__init__ = new_init
 
 setattr(DotMap, 'safe_deep_get', safe_deep_get)
 setattr(DotMap, 'safe_deep_set', safe_deep_set)
