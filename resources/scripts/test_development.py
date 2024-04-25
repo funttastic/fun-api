196980ab7efa67fa9b9d6dfe9fc008521769cae0
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-04-25 14:57:57 -0300

    Adding a new development python script.

---
+++ resources/scripts/test_development.py
@@ -0,0 +1,63 @@
+import os
+
+from pathlib import Path
+
+import requests
+import ssl
+import websocket
+
+username = "root"
+password = "asdf"
+client_cert = Path("../certificates/client_cert.pem").absolute().resolve().as_posix()
+client_key = Path("../certificates/client_key.pem").absolute().resolve().as_posix()
+
+ssl_defaults = ssl.get_default_verify_paths()
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+target_id = "fun-client.back"
+
+
+def sign_in(username, password):
+	url = "https://localhost:30001/auth/signIn"
+
+	credentials = {"username": username, "password": password}
+
+	response = requests.post(
+		url,
+		json=credentials,
+		verify=False,  # In a production environment, you should verify SSL certificates
+		cert=(client_cert, client_key)
+	)
+
+	if response.status_code == 200:
+		return response.json()["token"]
+	else:
+		raise Exception("Authentication failed")
+
+
+def	test_request(token):
+	url = "https://localhost:30001/development/test"
+	headers = {
+		"Authorization": f"Bearer {token}"
+	}
+	response = requests.get(
+		url,
+		headers=headers,
+		json={},
+		verify=False,  # In a production environment, you should verify SSL certificates
+		cert=(client_cert, client_key)
+	)
+	if response.status_code == 200:
+		return response.json()
+	else:
+		return None
+
+if __name__ == "__main__":
+	token = sign_in(username, password)
+	response = test_request(token)
+	if response:
+		print(response)
+	else:
+		print("Request failed")
