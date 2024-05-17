0fe30533881b02e0f6ec9a3766ea36e8170f37e9
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-05-17 10:48:46 -0300

    Remving v2 strategy references from production.

--- hummingbot/strategies/pure_market_making/v_2_0_0/workers/hb_gateway/kujira.py
+++
@@ -1,7 +0,0 @@
-from core.decorators import log_class_exceptions
-from hummingbot.strategies.pure_market_making.v_2_0_0.workers.base import WorkerBase
-
-
-@log_class_exceptions
-class HBGatewayKujiraWorker(WorkerBase):
-	pass
--- hummingbot/strategies/types.py
+++ hummingbot/strategies/types.py
@@ -1,12 +1,10 @@
 from enum import Enum
 
 from hummingbot.strategies.pure_market_making.v_1_0_0.supervisor import Supervisor as PureMarketMaking_1_0_0
-from hummingbot.strategies.pure_market_making.v_2_0_0.supervisor import Supervisor as PureMarketMaking_2_0_0
 
 
 class Strategy(Enum):
 	PURE_MARKET_MAKING_1_0_0 = PureMarketMaking_1_0_0
-	PURE_MARKET_MAKING_2_0_0 = PureMarketMaking_2_0_0
 
 	@staticmethod
 	def from_id_and_version(id: str, version: str):
