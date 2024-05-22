ef5fa142a2d60dd117c959fec4425a1fc281e4ce
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-05-22 15:58:37 -0300

    Updating test_development.py

--- resources/scripts/test_development.py
+++ resources/scripts/test_development.py
@@ -1,13 +1,10 @@
-import os
-
-from pathlib import Path
-
 import requests
 import ssl
-import websocket
+from pathlib import Path
 
 username = "root"
 password = "asdf"
+ca_cert = Path("../certificates/ca_cert.pem").absolute().resolve().as_posix()
 client_cert = Path("../certificates/client_cert.pem").absolute().resolve().as_posix()
 client_key = Path("../certificates/client_key.pem").absolute().resolve().as_posix()
 
@@ -27,7 +24,7 @@ def sign_in(username, password):
 	response = requests.post(
 		url,
 		json=credentials,
-		verify=False,  # In a production environment, you should verify SSL certificates
+		verify=ca_cert,  # In a production environment, you should verify SSL certificates
 		cert=(client_cert, client_key)
 	)
 
@@ -46,11 +43,11 @@ def	test_request(token):
 		url,
 		headers=headers,
 		json={},
-		verify=False,  # In a production environment, you should verify SSL certificates
+		verify=ca_cert,  # In a production environment, you should verify SSL certificates
 		cert=(client_cert, client_key)
 	)
 	if response.status_code == 200:
-		return response.json()
+		return response
 	else:
 		return None
 
